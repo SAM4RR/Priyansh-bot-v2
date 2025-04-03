55cc4f9ca5c26b130b90fdad0fa290e9364edd19
Samar Khan <samarkhaann4@gmail.com>
2025-04-04 01:30:39 +0300

    Update 0_SAMEER-GULABJAMUN.js

--- Priyansh/commands/0_SAMEER-GULABJAMUN.js
+++ Priyansh/commands/0_SAMEER-GULABJAMUN.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("julabjamun")==0 || event.body.indexOf("GULABJAMUN")==0 || event.body.indexOf("Gulabjamun")==0 || event.body.indexOf("sargulla")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐚𝐦𝐞𝐞𝐫 𝐊𝐇𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐆𝐮𝐥𝐚𝐁𝐣𝐚𝐌𝐮𝐧 \n< ────────────────── >",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/pinterest__1740148058555_GULAAB JAMUN 720x1280.mp4`)
+                                attachment: fs.createReadStream(__dirname + `/noprefix/e183fe6b2efec6b3f1974b405b981235.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😋", event.messageID, (err) => {}, true)
